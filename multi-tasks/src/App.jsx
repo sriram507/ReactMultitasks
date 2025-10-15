10fc9b252cfec943df3d16872c1fb7463ef46e95
sriram507 <isreeram2014@gmail.com>
2025-10-15 15:52:44 +0530

    task1

---
+++ multi-tasks/src/App.jsx
@@ -0,0 +1,8 @@
+// src/App.jsx
+import Task1 from "./task1/Task1";
+
+function App() {
+  return <Task1 />;
+}
+
+export default App;
---
+++ multi-tasks/src/task1/Task1.jsx
@@ -0,0 +1,9 @@
+// src/task1/Task1.jsx
+export default function Task1() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Task 1</h1>
+      <p>This is your first task!</p>
+    </div>
+  );
+}
